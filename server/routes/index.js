35132504547894ed6472aa72f85db8806db5e267
Braiden Parkinson <braidencparkinson@gmail.com>
2022-03-25 13:15:21 -0600

    clean up

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.send("welcome to the task manager api");
 });
 
 module.exports = router;
